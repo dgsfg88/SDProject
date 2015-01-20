d9c7323e088115e8eb5d665b55aec4492a9b43c8
Andrea <andreaianni@hotmail.com>
2015-01-20 12:08:51 +0100

    bug fixes

--- RMIRegistryStarter/src/EntryPoint.java
+++ RMIRegistryStarter/src/EntryPoint.java
@@ -8,7 +8,7 @@ public class EntryPoint {
 
 	public static void main(String[] args) {
 		try {
-			LocateRegistry.createRegistry(1099);
+			LocateRegistry.createRegistry(80);
 			new IncomingServer("1099");
 		} catch (RemoteException e1) {
 			e1.printStackTrace();
